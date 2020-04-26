300af1ee039a0a2f9956a8026878a887f215d603
paulstringer <pstringer@equalexperts.com>
2020-04-26 13:45:44 +0100

    [DEV] Refactors HP35 Subtract and Addition functions

--- HP35FitnesseHost/FixtureExample.swift
+++ HP35FitnesseHost/FixtureExample.swift
@@ -7,8 +7,9 @@ class HP35Driver : NSObject {
     let hp35 = HP35Calculator()
 
     @objc
-    func press(_ key: String) {
-        hp35.keyPress(key)
+    func press(_ rawValue: String) {
+        if let digitKey = Int(rawValue) { hp35.press(digitKey) }
+        if let functionKey = HP35Calculator.FunctionKey(rawValue: rawValue) { hp35.press(functionKey) }
     }
 
     @objc
@@ -20,41 +21,29 @@ class HP35Driver : NSObject {
 
 class HP35Calculator {
 
-    public enum DigitKey: String {
-        case one = "1", two = "2", three = "3"
-    }
-
-    public enum ArithmeticKey: String {
-        case ENTER, PLUS = "+", SUBTRACT = "-"
+    public enum FunctionKey: String {
+        case ENTER, PLUS = "+", MINUS = "-"
     }
 
     private var y : Int?
     private var x = 0
 
-    private func press(_ key: DigitKey) {
+    public func press(_ digit: Int) {
         if y == nil {
-            x = Int( "\(x)\(key.rawValue)" ) ?? 0
+            x =  Int( "\(x)\(digit)" ) ?? 0
         } else {
-            x = Int(key.rawValue)!
+            x = digit
         }
     }
 
-    private func press (_ key: ArithmeticKey) {
+    public func press(_ key: FunctionKey) {
         switch key {
         case .ENTER:
             y = x
         case .PLUS:
-            x = (y ?? 0) + x
-        case .SUBTRACT:
+            x += (y ?? 0 )
+        case .MINUS:
             x = (y ?? 0) - x
-        }
-    }
-
-    public func keyPress(_ key: String) {
-        if let digitKey = DigitKey(rawValue: key) {
-            press(digitKey)
-        } else if let arithmeticKey = ArithmeticKey(rawValue: key ) {
-            press(arithmeticKey)
         }
     }
 
